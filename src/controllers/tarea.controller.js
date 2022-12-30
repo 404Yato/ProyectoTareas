630522879fb8c3c15e8126c05ca15f612fe740a7
victor aedo <vict.aedo@duocuc.cl>
2022-12-30 00:47:12 -0300

    getTareas

--- src/controllers/tarea.controller.js
+++ src/controllers/tarea.controller.js
@@ -1,5 +1,6 @@
 const db = require("../config/db/config")
 
+const { fnObtenerTareas } = require("../config/storedFunction/call/tarea");
 const {deleteTarea: fnEliminarTarea} = require('../config/storedFunction/call/tarea/eliminar_tarea')
 const db = require("../config/db/config");
 const { fnObtenerTareas } = require("../config/storedFunction/call/tarea");
@@ -33,6 +34,32 @@ const delTarea = async (req, res)=> {
         console.log(error)
     }
 }
+
+
+
+/**
+ * Obtien todos los usuarios del sistema
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getTareas = async (req, res) => {
+    try {
+
+        const resp = await fnObtenerTareas();
+
+        sendOk(res, `Tareas obtenidas correctamente`, resp);
+
+    } catch (error) {
+        internalError(res, `${error.message || 'error no controlado'}`, error);
+    }
+}
+
+
+
+
+
+
 module.exports = {
-    delTarea
+    delTarea,
+    getTareas
 }

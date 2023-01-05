2446e8afdffae74c5f8b8124021e9d9de7ed4f97
victor aedo <vict.aedo@duocuc.cl>
2023-01-05 01:20:43 -0300

    .

--- src/routes/tarea.route.js
+++ src/routes/tarea.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const defaultPath = process.env.DEFAULT_PATH_API;
 
 router.delete(`/${defaultPath}/tarea/delTarea/:idTarea`, tareaController.delTarea);
+router.get(`/${defaultPath}/tarea/obtener-Tarea`, tareaController.getTareas);
 
 
 module.exports = router;

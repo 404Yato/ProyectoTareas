2431b4ba31b7755254b163c862e9349f66f495cd
Hesllar <hesllar.linzmayer@gmail.com>
2023-01-23 23:03:24 -0300

    .

--- src/routes/rol.route.js
+++ src/routes/rol.route.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 const defaultPath = process.env.DEFAULT_PATH_API
 
-router.get(`/${defaultPath}/rol/obtener-roles`, [validarToken], rolController.getRoles);
+router.get(`/${defaultPath}/rol/obtener-roles`, rolController.getRoles);
 router.post(`/${defaultPath}/rol/crear_rol`, rolController.crearRols);
 
 module.exports = router
